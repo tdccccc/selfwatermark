09d0ef1a1c7b5ecdea5f4738b919e9a657567e8f
Da-chuan <202121160014@mai.bnu.edu.cn>
2024-01-13 23:33:46 +0800

    first commit: modified selfwatermark, tdccccc versiong

--- watermark.py
+++ watermark.py
@@ -1,158 +1,208 @@
-import exifread
-from PIL import Image, ImageDraw, ImageFont
+import os 
+import glob
+import cv2
+from decimal import Decimal
+from fractions import Fraction
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from PIL.ExifTags import TAGS
 from pathlib import Path
-def get_exif(IM):
-    f=open(IM,'rb')
-    tags=exifread.process_file(f)
-    keynd=['Image Orientation','Image Make','Image Model','Image Artist','EXIF ExposureTime','EXIF FNumber','EXIF ISOSpeedRatings','EXIF FocalLength','Image DateTime']
-    dic={key:value for key, value in tags.items() if key in keynd}
-    if 'EXIF FNumber' in dict.keys(dic):
-        dic['EXIF FNumber']=float(eval(str(dic['EXIF FNumber'])))
-    for k in keynd[1:]:
-        if k not in dict.keys(dic):
-            dic[k]='-'
-    return dic
 
 
 
+def get_exif_info(image_path):
+    # 打开图像文件
+    image = Image.open(image_path)
+    # 获取 EXIF 数据
+    exif_data = image._getexif()
+    if exif_data is not None:
+        # 创建一个字典来存储所需的信息
+        exif_info = {}
+        # 遍历 EXIF 数据
+        for tag, value in exif_data.items():
+            tag_name = TAGS.get(tag, tag)
+            exif_info[tag_name] = value
+        return exif_info
+    else:
+        print("No EXIF data found in the image.")
 
 
-
-
-
-class photo:
+class Photo:
     def __init__(self,path):
-        self.path=path
-
-    @property
-    def name(self):
-        return(str(self.path).split('\\')[-1])
-    @property
-    def exif_dic(self):
-        return(get_exif(self.path))
-    @property
-    def mkrstr(self):
-        return(str(self.exif_dic['Image Make']))
-    @property
-    def mdlstr(self):
-        return(str(self.exif_dic['Image Model']))
-    @property
-    def artstr(self):
-        return(str(self.exif_dic['Image Artist']))
-    @property
-    def ori(self):
-        if 'Image Orientation' in dict.keys(self.exif_dic):
-            return(self.exif_dic['Image Orientation'].values[0])
-        else:
-            return('-')
-    @property
-    def exstr(self):
-        return(str(self.exif_dic['EXIF ExposureTime'])+'s'+'  f/'+str(self.exif_dic['EXIF FNumber'])+'  ISO '+str(self.exif_dic['EXIF ISOSpeedRatings'])+"  "+str(self.exif_dic['EXIF FocalLength'])+'mm')
-    @property
-    def tmstr(self):
-        time=str(self.exif_dic['Image DateTime']).split(":")
-        if time[0] !='-':
-            tm=time[0]+"/"+time[1]+"/"+time[2]+":"+time[3]+":"+time[4]
-        else: 
-            tm = '-'
-        return(tm)
+        self.path = path
+        self.name = os.path.basename(self.path)
+        self.exif_info = get_exif_info(self.path)
+    
     @property
     def img(self):
-        iminit=Image.open(self.path)
-        i=self.ori
-        if i==8:
-            im=iminit.transpose(Image.Transpose.ROTATE_90)
-        elif i==7:
-            imi=iminit.transpose(Image.Transpose.ROTATE_270)
-            im=imi.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
-        elif i==6:
-            im=iminit.transpose(Image.Transpose.ROTATE_270)
-        elif i==5:
-            imi=iminit.transpose(Image.Transpose.ROTATE_270)
-            im=imi.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
-        elif i==4:
-            im=iminit.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
-        elif i==3:
-            im=iminit.transpose(Image.Transpose.ROTATE_180)
-        elif i==2:
-            im=iminit.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
-        else:
-            im=iminit
-        return(im)
+        # img = Image.open(self.path)
+        # if 'Orientation' in self.exif_info:
+        #     orientation = int(self.exif_info['Orientation'])
+        #     if orientation==1:
+        #         pass
+        #     if orientation==8:
+        #         img = img.rotate(90)
+        img = cv2.imread(self.path)
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        return img
 
+    @property
+    def artist(self):
+        return str(self.exif_info['Artist'])
 
-
-    def Pana_add_text(self):
-        IM=self.img
-        xsize , ysize =IM.size
-        newim=Image.new(mode="RGB",size=(xsize,ysize+450),color=(255,255,255))
-        newim.paste(IM)
-        ft=ImageFont.truetype("arial.ttf",125)
-        ftime=ImageFont.truetype("GARA.TTF",125)
-        logo=Image.open('logo\\Lumix_Logo.jpg')
-        newim.paste(logo,(75,ysize+150))
-        LaIm=ImageDraw.Draw(newim)
-        LaIm.text((1200,ysize+50),self.mkrstr,(0,0,0),ft)
-        LaIm.text((1200,ysize+250),self.mdlstr,(0,0,0),ft)
-        LaIm.text((xsize-1700,ysize+50),self.exstr,(0,0,0),ft)
-        LaIm.text((xsize-1050,ysize+250),self.tmstr,(0,0,0),ftime)
-        LaIm.text((700,ysize+50),self.artstr,(0,0,0),ft)
-        return(newim)
+    @property
+    def camera_manufacturer(self):
+        return str(self.exif_info['Make'])
     
+    @property
+    def camera_model(self):
+        return str(self.exif_info['Model'])
     
+    @property
+    def lens_model(self):
+        if 'LensModel' in self.exif_info:
+            return str(self.exif_info['LensModel'])
     
-    def Sony_add_text(self):
-        IM=self.img
-        xsize , ysize =IM.size
-        newim=Image.new(mode="RGB",size=(xsize,ysize+450),color=(255,255,255))
-        newim.paste(IM)
-        ft=ImageFont.truetype("arial.ttf",125)
-        ftime=ImageFont.truetype("GARA.TTF",125)
-        logo=Image.open('logo\\sony-alpha.jpg')
-        newim.paste(logo,(75,ysize+150))
-        LaIm=ImageDraw.Draw(newim)
-        LaIm.text((600,ysize+50),self.mkrstr,(0,0,0),ft)
-        LaIm.text((600,ysize+250),self.mdlstr,(0,0,0),ft)
-        LaIm.text((xsize-1700,ysize+50),self.exstr,(0,0,0),ft)
-        LaIm.text((xsize-1050,ysize+250),self.tmstr,(0,0,0),ftime)
-        LaIm.text((700,ysize+50),self.artstr,(0,0,0),ft)
-        return(newim)
+    @property
+    def lens_focal_length(self):
+        if 'FocalLength' in self.exif_info:
+            focal_length = int(self.exif_info['FocalLength'])
+            return str(str(focal_length) + 'mm')
+    
+    @property
+    def exp_aperture(self):
+        if 'FNumber' in self.exif_info:
+            fnumber = self.exif_info['FNumber']
+            return str('f/' + '%.1f'%(fnumber))
+    
+    @property
+    def exp_shutterspeed(self):
+        if 'ExposureTime' in self.exif_info:
+            tmp = self.exif_info['ExposureTime']
+            tmp2 = Fraction(Decimal(str(tmp))).limit_denominator()
+            return str(tmp2)
+    
+    @property
+    def exp_iso(self):
+        if 'ISOSpeedRatings' in self.exif_info:
+            iso = int(self.exif_info['ISOSpeedRatings'])
+            return str('ISO' + str(iso))
+    
+    @property
+    def datetime(self):
+        if 'DateTimeOriginal' in self.exif_info:
+            datetime = self.exif_info['DateTimeOriginal']
+            date = datetime.split(' ')[0].split(':')
+            time = datetime.split(' ')[1].split(':')
+            return str(date[0] + '-' + date[1] + '-' + date[2] + '  ' +
+                    time[0] + ':' + time[1])
+    
+    @property
+    def watermark_info(self):
+        # Define the text to be printed
+        left_info = [
+            f"{self.camera_manufacturer} {self.camera_model}",
+            f"{self.lens_model}"
+        ]
+        right_info = [
+            f"{self.lens_focal_length} {self.exp_aperture} {self.exp_shutterspeed} {self.exp_iso}",
+            f"{self.datetime}"
+        ]
+        return left_info, right_info
+    
+    @property
+    def watermark_logo(self):
+        band = self.camera_manufacturer.lower()
+        logo_dir = 'logo/'
+        pattern = os.path.join(logo_dir, f"*{band}*")
+        matching_files = glob.glob(pattern, recursive=True)
+        logo_img = Image.open(matching_files[0])
+        return logo_img
+
+    def add_watermark(self):
+        img = self.img
+        width, height = img.size
+        left_text, right_text = self.watermark_info
+        logo_img = self.watermark_logo
+        
+        # calculate border size based on the image size
+        border_percentage = 0.03
+        border_short = int(min(width, height)*border_percentage)
+        border_long = border_short * 4
+
+        # create a new image with a white border
+        new_size = (width + 2*border_short, height + border_short + border_long)
+        new_img = Image.new("RGB", new_size, "white")
+        new_img.paste(img, (border_short, border_short))
+
+        # prepare to draw text on the image
+        draw = ImageDraw.Draw(new_img)
+        text_height = border_short*0.75
+        font_small = ImageFont.truetype('fonts/NotoSansSC-Light.ttf', text_height)
+        font_large = ImageFont.truetype('fonts/NotoSansSC-Bold.ttf', text_height)
+
+        # draw text
+        left_text_position = (int(border_short*2), 
+                              int(height + border_short*2))
+        right_text_width = draw.textlength(right_text[0], font=font_large)
+        right_text_position = (int(new_size[0] - right_text_width - border_short*2), 
+                               int(height + border_short*2))
+
+        line_height = border_short * 1
+        for i, line in enumerate(left_text):
+            if i == 0:
+                draw.text(left_text_position, line, font=font_large, fill="black")
+            else:
+                draw.text(left_text_position, line, font=font_small, fill="black")
+            left_text_position = (left_text_position[0], left_text_position[1] + line_height)
+        for i, line in enumerate(right_text):
+            if i == 0:
+                draw.text(right_text_position, line, font=font_large, fill="black")
+            else:
+                draw.text(right_text_position, line, font=font_small, fill="black")
+            right_text_position = (right_text_position[0], right_text_position[1] + line_height)
+
+        # draw cutting line
+        line_start = (int(right_text_position[0] - border_short), 
+                      int(right_text_position[1]))
+        line_end = (int(right_text_position[0] - border_short), 
+                    int(right_text_position[1] - line_height * len(right_text)))
+        draw.line([line_start, line_end], fill="black", width=int(border_short*0.05))
+
+        # logo resize
+        logo_height = int(border_short * 0.75)
+        logo_width = int((logo_height / logo_img.height) * logo_img.width)
+        logo_img = logo_img.resize((logo_width, logo_height))
+
+        # draw logo
+        logo_position = (int(right_text_position[0] - logo_width - border_short*2),
+                         int(height + border_short * 2.7))
+        new_img.paste(logo_img, logo_position)
+
+        new_img.save('output/%s'%(self.name))
     
 
-    def Oth_add_text(self):
-        IM=self.img
-        xsize , ysize =IM.size
-        newim=Image.new(mode="RGB",size=(xsize,ysize+450),color=(255,255,255))
-        newim.paste(IM)
-        ft=ImageFont.truetype("arial.ttf",125)
-        ftime=ImageFont.truetype("GARA.TTF",125)
-        LaIm=ImageDraw.Draw(newim)
-        LaIm.text((100,ysize+50),self.mkrstr,(0,0,0),ft)
-        LaIm.text((100,ysize+250),self.mdlstr,(0,0,0),ft)
-        LaIm.text((xsize-1700,ysize+50),self.exstr,(0,0,0),ft)
-        LaIm.text((xsize-1050,ysize+250),self.tmstr,(0,0,0),ftime)
-        return(newim)
-    
+def get_jpg_files(folder_path):
+    folder_path = Path(folder_path)
+    if str(os.name) == 'nt':
+        jpg_files = list(folder_path.glob('**\\*.jpg'))
+        JPG_files = list(folder_path.glob('**\\*.JPG'))
+        jpeg_files = list(folder_path.glob('**\\*.jpeg'))
+    if str(os.name) == 'posix':
+        jpg_files = glob.glob(os.path.join(folder_path, '*.jpg'))
+        JPG_files = glob.glob(os.path.join(folder_path, '*.JPG'))
+        jpeg_files = glob.glob(os.path.join(folder_path, '*.jpeg'))
+    return jpg_files+JPG_files+jpeg_files
 
 
-    def add_text(self):
-        if self.mkrstr=='SONY':
-            im=self.Sony_add_text()
-        elif self.mkrstr=='Panasonic':
-            im=self.Pana_add_text()
-        else:
-            im=self.Oth_add_text()
-        return(im)
-    
-
-def process(file_path):
-    li=Path(file_path)
-    Li=list(li.glob('**\\*.JPG'))
-    for i in range(0,len(Li)):
-        a=photo(Li[i])
-        print(a.mkrstr)
-        a.add_text().save('output\\'+a.name)
+def process(folder_path):
+    img_files = get_jpg_files(folder_path)
+    img_num = len(img_files)
+    for i in range(img_num):
+        img = Photo(img_files[i])
+        print(img.camera_manufacturer)
+        img.add_watermark()
 
 
-paths=input("input folder path to add watermark//请输入需要添加水印的文件夹")
-process(paths)
+if __name__ == '__main__':
+    folder_path  = 'image/'
+    process(folder_path)
